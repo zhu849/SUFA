b4aca1666a6260b608abca98765c17ca83a2c792
zhu849 <66557634+zhu849@users.noreply.github.com>
2020-12-29 12:13:56 +0800

    Update comment

--- classify.py
+++ classify.py
@@ -9,49 +9,43 @@ counter = 0
 # color var 
 green = np.uint8([[[0, 255, 0]]])  #green color
 hsvGreen = cv2.cvtColor(green, cv2.COLOR_BGR2HSV) #hsv value of green color 
+red = np.uint8([[[0, 0, 255]]]) #red color
+hsvRed = cv2.cvtColor(red, cv2.COLOR_BGR2HSV) #hsv value of red color
 lowerLimitGreen = hsvGreen[0][0][0] - 10, 100, 100  # range of green color lower limit and upper limit
 upperLimitGrenn = hsvGreen[0][0][0] + 10, 255, 255
+lowerLimitRed = hsvred[0][0][0] - 10, 100, 100 # range of red color lower limit and upper limit
+upperLimitRed = hsvred[0][0][0] + 10, 255, 255
 #range of green color
 lg = np.array(lowerLimit) 
 ug = np.array(upperLimit)
-
-red = np.uint8([[[0, 0, 255]]]) #red color
-hsvRed = cv2.cvtColor(red, cv2.COLOR_BGR2HSV) #hsv value of red color
-lowerLimitRed = hsvred[0][0][0] - 10, 100, 100 # range of red color lower limit and upper limit
-upperLimitRed = hsvred[0][0][0] + 10, 255, 255
 #range of red color
 lr = np.array(lowerLimitRed) 
 ur = np.array(upperLimitRed)
 
-def classifyGR():
+def classifyGR(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #convert the image into hsv
-
     green_mask = cv2.inRange(hsv, lg, ug) #green masked image
     cv2.imshow('green', green_mask) #show the image 
-    
     red_mask = cv2.inRange(hsv, lr, ur) #red masked image
     cv2.imshow('red', red_mask)  #show the image
-
+    # counter which color more
     green_counter = 0
     red_counter = 0
-
     for i in range(len(green_mask)):
         for j in range(len(green_mask[i])):
             if(green_mask[i][j] == 0):
                 green_counter = green_counter + 1
-    
     for i in range(len(red_mask)):
         for j in range(len(red_mask[i])):
             if(red_mask[i][j] == 0):
                 red_counter = red_counter + 1
+
     if(green_counter < red_counter):
         print('land')
-        my_command = 'land'
-        socket.sendto(my_command.encode('utf-8'), tello_address)
+        socket.sendto('land'.encode('utf-8'), tello_address)
     
     print("g",green_counter)
     print("r",red_counter)
-
 
     
 if __name__ == '__main__':
