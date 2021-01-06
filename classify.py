689118a5266ad75cfc4bb580d6e1bc18766493e5
chihovrflo <chihovrflo@chihovrflode-MacBook-Air.local>
2021-01-06 13:19:01 +0800

    classify test

--- classify.py
+++ classify.py
@@ -1,23 +1,59 @@
 import socket
-import time
 import cv2
 import numpy as np
-import threading
+from threading import Thread
+import time
+import os
+import datetime
 
-##### golbal var #####
-counter = 0
+class ThreadedCamera(object):
+    
+    def __init__(self, src=0):
+        self.capture = cv2.VideoCapture(src)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+
+        # FPS = 1/X
+        # X = desired FPS
+        self.FPS = 1/30
+        self.FPS_MS = int(self.FPS * 1000)
+
+        # Start frame retrieval thread
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def update(self):
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+            time.sleep(self.FPS)
+    '''
+    def show_frame(self):
+        if(cv2.waitKey(1)&0xFF == ord ('q')):
+            self.capture.release ()
+        cv2.imshow('frame', self.frame)
+        classifyGR(self.frame)
+        cv2.waitKey(self.FPS_MS)
+    '''
+    
+    def check_frame(self):
+        if(cv2.waitKey(1)&0xFF == ord ('q')):
+            self.capture.release ()
+        classifyGR(self.frame)
+        cv2.waitKey(self.FPS_MS)
+
 # color var 
 green = np.uint8([[[0, 255, 0]]])  #green color
 hsvGreen = cv2.cvtColor(green, cv2.COLOR_BGR2HSV) #hsv value of green color 
 red = np.uint8([[[0, 0, 255]]]) #red color
 hsvRed = cv2.cvtColor(red, cv2.COLOR_BGR2HSV) #hsv value of red color
 lowerLimitGreen = hsvGreen[0][0][0] - 10, 100, 100  # range of green color lower limit and upper limit
-upperLimitGrenn = hsvGreen[0][0][0] + 10, 255, 255
-lowerLimitRed = hsvred[0][0][0] - 10, 100, 100 # range of red color lower limit and upper limit
-upperLimitRed = hsvred[0][0][0] + 10, 255, 255
+upperLimitGreen = hsvGreen[0][0][0] + 10, 255, 255
+lowerLimitRed = hsvRed[0][0][0] - 10, 100, 100 # range of red color lower limit and upper limit
+upperLimitRed = hsvRed[0][0][0] + 10, 255, 255
 #range of green color
-lg = np.array(lowerLimit) 
-ug = np.array(upperLimit)
+lg = np.array(lowerLimitGreen) 
+ug = np.array(upperLimitGreen)
 #range of red color
 lr = np.array(lowerLimitRed) 
 ur = np.array(upperLimitRed)
@@ -25,9 +61,9 @@ ur = np.array(upperLimitRed)
 def classifyGR(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #convert the image into hsv
     green_mask = cv2.inRange(hsv, lg, ug) #green masked image
-    cv2.imshow('green', green_mask) #show the image 
+    #cv2.imshow('green', green_mask) #show the image 
     red_mask = cv2.inRange(hsv, lr, ur) #red masked image
-    cv2.imshow('red', red_mask)  #show the image
+    #cv2.imshow('red', red_mask)  #show the image
     # counter which color more
     green_counter = 0
     red_counter = 0
@@ -39,10 +75,10 @@ def classifyGR(img):
         for j in range(len(red_mask[i])):
             if(red_mask[i][j] == 0):
                 red_counter = red_counter + 1
-
-    if(green_counter < red_counter):
-        print('land')
-        socket.sendto('land'.encode('utf-8'), tello_address)
+    
+    if(green_counter > red_counter):
+        print('forward')
+        socket.sendto('forward 40'.encode('utf-8'), tello_address)
     
     print("g",green_counter)
     print("r",red_counter)
@@ -59,26 +95,20 @@ if __name__ == '__main__':
     socket.sendto ('command'.encode (' utf-8 '), tello_address)
     socket.sendto ('streamon'.encode (' utf-8 '), tello_address)
     print ("Start streaming: ")
-    capture = cv2.VideoCapture ('udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=50000000',cv2.CAP_FFMPEG)
-    if not capture.isOpened():
-        capture.open('udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=50000000',cv2.CAP_FFMPEG)
-
+    
+    src = 'udp://0.0.0.0:11111?overrun_nonfatal=1&fifo_size=50000000'
+    threaded_camera = ThreadedCamera(src)
+    
     # tello start operation
     socket.sendto('takeoff'.encode('utf-8'), tello_address)
-
+    
     while True:
-        counter = counter + 1
-        ret, frame = capture.read()
-        if(ret):
-            cv2.imshow('frame', frame)
-        if(counter%30==0):
-            t = threading.Thread(target = classifyGR, args = (frame))
-            t.start()
-            #classifyGR(frame)
+        try:
+            #threaded_camera.show_frame()
+            threaded_camera.check_frame()
+        except AttributeError:
+            pass
         if(cv2.waitKey(1)&0xFF == ord ('q')):
-            break
-        
-    capture.release ()
-    cv2.destroyAllWindows ()
-    socket.sendto ('streamoff'.encode (' utf-8 '), tello_address)
-    socket.close()
+            cv2.destroyAllWindows ()
+            socket.sendto ('streamoff'.encode (' utf-8 '), tello_address)
+            socket.close()
